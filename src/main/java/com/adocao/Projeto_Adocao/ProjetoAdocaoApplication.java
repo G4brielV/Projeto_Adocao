bb78d57dd1d0bfb667bcb86e0e4cdc37639e6b97
Gabriel Vinícius <gabrielvto18@gmail.com>
2024-11-18 18:58:40 -0300

    Initial commit

---
+++ src/main/java/com/adocao/Projeto_Adocao/ProjetoAdocaoApplication.java
@@ -0,0 +1,13 @@
+package com.adocao.Projeto_Adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAdocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAdocaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adocao/Projeto_Adocao/ProjetoAdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adocao.Projeto_Adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
