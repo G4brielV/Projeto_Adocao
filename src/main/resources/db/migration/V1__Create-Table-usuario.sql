7ba0359291709987a6e19997d06a66e32b860a67
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-01-06 16:42:47 -0300

    Saldo em Centavos

--- src/main/resources/db/migration/V1__Create-Table-usuario.sql
+++ src/main/resources/db/migration/V1__Create-Table-usuario.sql
@@ -2,9 +2,9 @@ CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
     ativo BOOLEAN DEFAULT TRUE,
     nome VARCHAR(255) NOT NULL,
-    identificador VARCHAR(18) NOT NULL UNIQUE, -- Garantia no banco
+    identificador VARCHAR(18) NOT NULL, -- Armazena CPF ou CNPJ
     email VARCHAR(255) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
     telefone VARCHAR(15),
-    saldo NUMERIC(10, 2) DEFAULT 0.00
+    saldo BIGINT DEFAULT 0 -- Saldo armazenado em centavos (ex.: 10000 = R$ 100,00)
 );
