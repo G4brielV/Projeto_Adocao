4901d548c4e313cd189cdd4192d25f654fab1a5a
Gabriel Vinícius <gabrielvto18@gmail.com>
2025-01-10 19:01:53 -0300

    especie -> raca

--- src/main/resources/db/migration/V2__Create-Table-endereco.sql
+++ src/main/resources/db/migration/V2__Create-Table-endereco.sql
@@ -8,6 +8,6 @@ CREATE TABLE endereco (
     estado VARCHAR(2),
     latitude NUMERIC(10, 8),
     longitude NUMERIC(11, 8),
-    usuario_id INT NOT NULL, -- Um usuário pode ter mais de um endereço
+    usuario_id INT NOT NULL, -- Um usuário não pode ter mais de um endereço
     FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE CASCADE
 );
--- src/main/resources/db/migration/V7__Create-Table-animal.sql
+++ src/main/resources/db/migration/V7__Create-Table-animal.sql
@@ -2,12 +2,12 @@ CREATE TABLE animal (
     id SERIAL PRIMARY KEY,
     ativo BOOLEAN DEFAULT TRUE,
     nome VARCHAR(255) NOT NULL,
-    especie VARCHAR(50) NOT NULL,
+    raca VARCHAR(50) NOT NULL,
     cor VARCHAR(50),
     porte VARCHAR(50),
     nascimento DATE,
     castracao BOOLEAN,
     descricao TEXT,
-    dono_id INT NOT NULL, -- Referência ao usuário que cadastrou o animal
-    FOREIGN KEY (dono_id) REFERENCES usuario (id) ON DELETE CASCADE
+    dono_id INT NOT NULL, -- Referência ao usuario que cadastrou o animal
+    FOREIGN KEY (dono_id) REFERENCES usuario (id)
 );
